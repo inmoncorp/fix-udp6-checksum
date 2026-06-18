9466eab99da3ca2d45fc6948963349779500906f
Neil McKee <neil.mckee@inmon.com>
2026-06-18 12:11:16 -0700

    improve tc install/uninstall commands to be more specific

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 sudo tc qdisc add dev "$1" clsact
-sudo tc filter add dev "$1" ingress bpf da obj fix_checksum.o sec tc/ingress
+sudo tc filter add dev "$1" ingress pref 1 bpf da obj fix_checksum.o sec tc/ingress
 
--- uninstall.sh
+++ uninstall.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-sudo tc filter del dev "$1" ingress pref 1
+sudo tc filter del dev "$1" ingress protocol all pref 1 handle 0x1 bpf
 sudo tc qdisc del dev "$1" clsact
 
