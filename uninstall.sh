28a8a8da952f718a35ed38184ae4d8b067e1405e
Neil McKee <neil.mckee@inmon.com>
2026-06-18 15:58:04 -0700

    fix uninstall.sh now that the filter is more selective

--- uninstall.sh
+++ uninstall.sh
@@ -3,6 +3,6 @@ if (( $# != 1 )); then
     echo "usage: $0 <interface>"
     exit 1;
 fi
-sudo tc filter del dev "$1" ingress protocol all pref 1 handle 0x1 bpf
+sudo tc filter del dev "$1" ingress protocol ipv6 pref 1 u32
 sudo tc qdisc del dev "$1" clsact
 
